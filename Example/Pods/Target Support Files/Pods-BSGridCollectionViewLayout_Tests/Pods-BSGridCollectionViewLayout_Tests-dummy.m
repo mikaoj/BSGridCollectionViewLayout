2fac63db0248b67c3d8798f2750248954d266c73
Joakim Gyllström <joakim@backslashed.se>
2018-12-27 12:26:27 +0100

    Moved away from cocoapods project structure.

--- Example/Pods/Target Support Files/Pods-BSGridCollectionViewLayout_Tests/Pods-BSGridCollectionViewLayout_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BSGridCollectionViewLayout_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_BSGridCollectionViewLayout_Tests
-@end
--- Example/Pods/Target Support Files/Pods-BSGridCollectionViewLayout_Tests/Pods-BSGridCollectionViewLayout_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_BSGridCollectionViewLayout_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_BSGridCollectionViewLayout_TestsVersionString[];
-
